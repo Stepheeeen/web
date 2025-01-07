9b3b8216fd9dfa04c9b1b39646d04dd54553aa73
stepheeeen <stephenon56@gmail.com>
2025-01-07 13:42:59 +0100

    Add meta tags for SEO optimization in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Flair Technologies LTD specializes in cutting-edge tech solutions, groundbreaking innovations, and transforming ideas into impactful realities. Join us in shaping the future with technology."
+        />
+        <meta
+          name="keywords"
+          content="Flair Technologies, tech solutions, innovative technology, digital transformation, groundbreaking innovations, future technology"
+        />
+        <meta name="author" content="Flair Technologies LTD" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
